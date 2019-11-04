df916e18ccc709d60dabd8aa2d0724dc03e982a1
Amy Stefani <amystefani1@gmail.com>
2019-11-04 10:25:51 -0800

    icmp ping functional

--- icmp-ping.py
+++ icmp-ping.py
@@ -8,18 +8,19 @@ import binascii
 ICMP_ECHO_REQUEST = 8
 
 
-def checksum(str):
+def checksum(string):
+    string = bytearray(string)
     csum = 0
-    countTo = (len(str) / 2) * 2
+    countTo = (len(string) / 2) * 2
     count = 0
     while count < countTo:
-        thisVal = ord(str[count+1]) * 256 + ord(str[count])
+        thisVal = string[count+1] * 256 + string[count]
         csum = csum + thisVal
-        csum = csum & 0xffffffffL
+        csum = csum & 0xffffffff
         count = count + 2
-    if countTo < len(str):
-        csum = csum + ord(str[len(str) - 1])
-        csum = csum & 0xffffffffL
+    if countTo < len(string):
+        csum = csum + string[-1]
+        csum = csum & 0xffffffff
     csum = (csum >> 16) + (csum & 0xffff)
     csum = csum + (csum >> 16)
     answer = ~csum
@@ -38,9 +39,20 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
             return "Request timed out."
         timeReceived = time.time()
         recPacket, addr = mySocket.recvfrom(1024)
-        #Fill in start
+
+        # Fill in start
         # Fetch the ICMP header from the IP packet
-        #Fill in end
+        icmpHeader = recPacket[20:28]
+        icmpType, code, mychecksum, packetId, sequence = struct.unpack(
+            "bbHHh", icmpHeader)
+
+        if packetId == ID:
+            bytesInDouble = struct.calcsize("d")
+            timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
+            delay = timeReceived - timeSent
+            return delay
+        # Fill in end
+
         timeLeft = timeLeft - howLongInSelect
         if timeLeft <= 0:
             return "Request timed out."
@@ -57,10 +69,10 @@ def sendOnePing(mySocket, destAddr, ID):
     myChecksum = checksum(header + data)
     # Get the right checksum, and put in the header
     if sys.platform == 'darwin':
-        myChecksum = socket.htons(myChecksum) & 0xffff
+        myChecksum = htons(myChecksum) & 0xffff
         # Convert 16-bit integers from host to network byte order.
     else:
-        myChecksum = socket.htons(myChecksum)
+        myChecksum = htons(myChecksum)
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     packet = header + data
     # AF_INET address must be tuple, not str
@@ -70,33 +82,40 @@ def sendOnePing(mySocket, destAddr, ID):
 
 
 def doOnePing(destAddr, timeout):
-    icmp = socket.getprotobyname("icmp")
+    icmp = getprotobyname("icmp")
 
+    # SOCK_RAW is a powerful socket type. For more details see:
+    # http://sock-raw.org/papers/sock_raw
+    # Fill in start
+    # Create Socket here
+    mySocket = socket(AF_INET, SOCK_DGRAM, icmp)
+    # mySocket = socket(AF_INET, SOCK_RAW, icmp)
+    # Fill in end
 
-# SOCK_RAW is a powerful socket type. For more details see:
-# http://sock-raw.org/papers/sock_raw
-#Fill in start
-# Create Socket here
-#Fill in end
-myID = os.getpid() & 0xFFFF  # Return the current process i
-sendOnePing(mySocket, destAddr, myID)
-delay = receiveOnePing(mySocket, myID, timeout, destAddr)
-mySocket.close()
-return delay
+    myID = os.getpid() & 0xFFFF  # Return the current process i
+    sendOnePing(mySocket, destAddr, myID)
+    delay = receiveOnePing(mySocket, myID, timeout, destAddr)
+    mySocket.close()
+    return delay
 
 
 def ping(host, timeout=1):
-    # timeout=1 means: If one second goes by without a reply from the server,
-    # the client assumes that either the client’s ping or the server’s pong is lost
-    dest = socket.gethostbyname(host)
-    print "Pinging " + dest + " using Python:"
-    print ""
+    # timeout=1 means: If 1 second goes by without a reply from the server, the
+    # client assumes that either the client’s ping or the server’s pong is lost
+    dest = gethostbyname(host)
+    print("Pinging " + dest + " using Python:")
+    print("")
     # Send ping requests to a server separated by approximately one second
-    while 1:
+    while(1):
         delay = doOnePing(dest, timeout)
-        print delay
+        print(delay)
         time.sleep(1)  # one second
+    return delay
 
 
-return delay
-ping("www.poly.edu")
+# ping("127.0.0.1")
+# ping("www.northeastern.edu")
+# ping("www.australia.gov.au")
+# ping("www.governo.it")
+# ping("www.japan.go.jp")
+ping("www.bundesregierung.de")
